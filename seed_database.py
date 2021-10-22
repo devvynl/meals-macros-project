3d9e7ffe347ed28c7ef50480e17727d6c276bdad
Devvyn <devvyn@gmail.com>
2021-10-22 02:47:27 +0000

    added to requirements and seed_database

---
+++ seed_database.py
@@ -0,0 +1,9 @@
+""" dropdb, createdb, use db.create_all to create tables, and  populate the db w/data """
+ 
+import os 
+import json 
+from datetime import datetime 
+
+import crud 
+import model
+import server 
