0a3878dd5197e3e5aa888647121831723b32f680
Devvyn <devvyn@gmail.com>
2021-10-22 04:17:29 +0000

    updated server.py

--- server.py
+++ server.py
@@ -3,8 +3,12 @@
 from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db
 import crud
+import requests
+
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+
+API_KEY = os.environ['MYFITNESSPAL_KEY']
