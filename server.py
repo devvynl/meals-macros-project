c043b2c81e8feff6fcac38789659ec46199a917b
Devvyn <devvyn@gmail.com>
2021-10-23 21:43:53 +0000

    added html files

--- server.py
+++ server.py
@@ -12,3 +12,18 @@ app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
 API_KEY = os.environ['MYFITNESSPAL_KEY']
+
+@app.route('/')
+def homepage():
+    """show homepage"""
+    return render_template('homepage.html')
+
+@app.route('/food')
+def food():
+    """view food options"""
+    food = crud.create_food()
+    return render_template('food.html', food=food)
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0.", debug=True)
