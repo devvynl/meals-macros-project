5687a9cb38f2d58a8c9732ad94b97ae3c6814d7a
Devvyn <devvyn@gmail.com>
2021-10-23 21:26:50 +0000

    added to database

--- seed_database.py
+++ seed_database.py
@@ -14,3 +14,8 @@ os.system("createdb calories")
 model.connect_to_db(server.app)
 model.db.create_all()
 
+#load data from API/JSON file
+with open("data/calories_macros.json") as f:
+    project_data = json.loads(f.read())
+
+
