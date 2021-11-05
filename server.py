d4e3b455ef0ae17ee77b1edf27e8df9ca6a099d3
Devvyn <devvyn@gmail.com>
2021-11-05 03:42:31 +0000

    added API key

--- server.py
+++ server.py
@@ -3,15 +3,16 @@
 from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db
 import crud
-# import requests
+import os
+import requests
 
-from jinja2 import StrictUndefined
+# from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 app.secret_key = "dev"
-app.jinja_env.undefined = StrictUndefined
+# app.jinja_env.undefined = StrictUndefined
 
-# API_KEY = os.environ['MYFITNESSPAL_KEY']
+API_KEY = os.environ['MYFITNESSPAL_KEY']
 
 @app.route('/')
 def homepage():
